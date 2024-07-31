47ec34e214019e839b09a35310bbba38f6bf5327
gaunitz <gaunitz@wifa.uni-leipzig.de>
2024-07-31 16:58:16 +0200

    - replace av lib for using CUDA FFMpeg

--- disable-h264-nvidia-cuda-support.sh
+++ disable-h264-nvidia-cuda-support.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 cp ./codecs/h264_original.py ./venv/lib/site-packages/aiortc/codecs/h264.py
+# Reinstall with binaries
+./venv/bin/pip uninstall av
+./venv/bin/pip install av
--- enable-h264-nvidia-cuda-support.sh
+++ enable-h264-nvidia-cuda-support.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 cp ./venv/lib/site-packages/aiortc/codecs/h264.py ./codecs/h264_original.py
 cp ./codecs/h264_nvidia.py ./venv/lib/site-packages/aiortc/codecs/h264.py
+
+./venv/bin/pip uninstall av
+./venv/bin/pip install av --no-binary av
