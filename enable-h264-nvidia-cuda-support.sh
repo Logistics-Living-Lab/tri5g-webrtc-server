fe7ec3b2e79b5513a6cba05418d1b9a932c7d283
gaunitz <gaunitz@wifa.uni-leipzig.de>
2024-07-31 17:07:52 +0200

    - added yes flag to pip install

--- enable-h264-nvidia-cuda-support.sh
+++ enable-h264-nvidia-cuda-support.sh
@@ -2,5 +2,5 @@
 cp ./venv/lib/python3.10/site-packages/aiortc/codecs/h264.py ./codecs/h264_original.py
 cp ./codecs/h264_nvidia.py ./venv/lib/python3.10/site-packages/aiortc/codecs/h264.py
 
-./venv/bin/pip uninstall av
+./venv/bin/pip uninstall av -y
 ./venv/bin/pip install av --no-binary av
