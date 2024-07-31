c3570f7671da00d6327d0369ef44210adc18d1f5
gaunitz <gaunitz@wifa.uni-leipzig.de>
2024-07-31 17:06:55 +0200

    - updated paths to linux

--- disable-h264-nvidia-cuda-support.sh
+++ disable-h264-nvidia-cuda-support.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cp ./codecs/h264_original.py ./venv/lib/site-packages/aiortc/codecs/h264.py
+cp ./codecs/h264_original.py ./venv/lib/python3.10/site-packages/aiortc/codecs/h264.py
 # Reinstall with binaries
 ./venv/bin/pip uninstall av
 ./venv/bin/pip install av
