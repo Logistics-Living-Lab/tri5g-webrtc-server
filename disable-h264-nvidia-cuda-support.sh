4d39cfb61996fd31b652ded64e4ba242a7975992
gaunitz <gaunitz@wifa.uni-leipzig.de>
2024-08-19 18:05:18 +0200

    - use CUDA Encoder in media file for recording

--- disable-h264-nvidia-cuda-support.sh
+++ disable-h264-nvidia-cuda-support.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cp ./codecs/h264_original.py ./venv/lib/python3.10/site-packages/aiortc/codecs/h264.py
+cp ./codecs/media_original.py ./venv/lib/python3.10/site-packages/aiortc/contrib/media.py
 # Reinstall with binaries
 ./venv/bin/pip uninstall av
 ./venv/bin/pip install av
--- enable-h264-nvidia-cuda-support.sh
+++ enable-h264-nvidia-cuda-support.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 cp ./venv/lib/python3.10/site-packages/aiortc/codecs/h264.py ./codecs/h264_original.py
+cp ./venv/lib/python3.10/site-packages/aiortc/contrib/media.py ./codecs/media_original.py
 cp ./codecs/h264_nvidia.py ./venv/lib/python3.10/site-packages/aiortc/codecs/h264.py
+cp ./codecs/media_nvidia.py ./venv/lib/python3.10/site-packages/aiortc/contrib/media.py
 
 ./venv/bin/pip uninstall av -y
 ./venv/bin/pip install av --no-binary av
