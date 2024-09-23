9805ba300431c42b113f423bfd70200e91fa008c
gaunitz <gaunitz@wifa.uni-leipzig.de>
2024-09-23 20:48:00 +0200

    - use promises to check if images are rendered

--- html-files/app.js
+++ html-files/app.js
@@ -3,6 +3,7 @@
 
 $(document).ready(() => {
     let processing = false
+    let processedPhotoFileName = ""
     let latestPhotoOriginal
     let latestPhotoProcessed
 
@@ -16,7 +17,6 @@ $(document).ready(() => {
                     if (selected) {
                         $html.find('input[type="radio"]').prop('checked', true);
                     }
-
                     $("#modelOptionsDiv").append($html)
                 }
             }
@@ -29,7 +29,6 @@ $(document).ready(() => {
         const detectionImageOriginalElement = $("#detectionImageOriginal")
         const detectionImageProcessed = $("#detectionImageProcessed")
         detectionImageOriginalElement.off("load")
-        detectionImageProcessed.attr('src', latestPhotoOriginal)
         $("#processingOverlay").show()
         detectionImageProcessed.addClass('blinking');
     }
@@ -42,39 +41,80 @@ $(document).ready(() => {
         $("#processingOverlay").hide()
     }
 
+    function waitForImageToLoad(imgElement) {
+        return new Promise((resolve, reject) => {
+            // Check if the image is already loaded (for cached images)
+            if (imgElement.complete && imgElement.naturalHeight !== 0) {
+                resolve('Image already loaded.');
+            } else {
+                // Add an event listener for the 'load' event
+                imgElement.onload = () => {
+                    resolve('Image loaded successfully.');
+                };
+                // Add an error listener in case the image fails to load
+                imgElement.onerror = () => {
+                    reject('Image failed to load.');
+                };
+            }
+        })
+    }
+
+    function checkOriginalImageElement(data) {
+        return new Promise((resolve, reject) => {
+            if (data.original && data.original[0]) {
+                if (!processing && latestPhotoOriginal !== data.original[0]) {
+                    firstLoad = latestPhotoOriginal === undefined
+                    latestPhotoOriginal = data.original[0]
+                    processedPhotoFileName = latestPhotoOriginal
+                    if (!firstLoad) {
+                        const toast = $('#toast');
+                        toast.addClass('toast-show')
+                        setTimeout(() => {
+                            toast.removeClass('toast-show');
+                        }, 5000);
+                        const detectionImageOriginalElement = $("#detectionImageOriginal")
+                        const detectionImageProcessed = $("#detectionImageProcessed")
+
+                        detectionImageOriginalElement.on("load", onOriginalPhotoReceived)
+                        detectionImageOriginalElement.attr('src', latestPhotoOriginal)
+                        detectionImageProcessed.attr('src', latestPhotoOriginal)
+
+                        return waitForImageToLoad(detectionImageOriginalElement)
+                            .then(
+                                () => waitForImageToLoad(detectionImageProcessed)
+                            )
+                    }
+                }
+            }
+            return resolve()
+        })
+    }
+
+    function checkProcessedImageElement(data) {
+        return new Promise((resolve, reject) => {
+            if (data.processed && data.processed[0]) {
+                if (latestPhotoProcessed !== data.processed[0]) {
+                    firstLoad = latestPhotoProcessed === undefined
+                    latestPhotoProcessed = data.processed[0]
+                    if (!firstLoad && latestPhotoProcessed === latestPhotoOriginal.replace("original", "processed")) {
+                        console.log(latestPhotoProcessed)
+                        const detectionImageProcessed = $("#detectionImageProcessed")
+                        detectionImageProcessed.on("load", onProcessedPhotoReceived)
+                        detectionImageProcessed.attr('src', latestPhotoProcessed)
+                        return waitForImageToLoad(detectionImageProcessed)
+                    }
+                }
+            }
+            return resolve()
+        })
+    }
+
     function getLatestPhoto(callback) {
         $.ajax("/api/photo-files", {
             type: 'GET', success: (data) => {
-                if (!processing) {
-                    if (data) {
-                        if (data.original && data.original[0]) {
-                            if (latestPhotoOriginal !== data.original[0]) {
-                                firstLoad = latestPhotoOriginal === undefined
-                                latestPhotoOriginal = data.original[0]
-                                if (!firstLoad) {
-                                    const toast = $('#toast');
-                                    toast.addClass('toast-show')
-                                    setTimeout(() => {
-                                        toast.removeClass('toast-show');
-                                    }, 5000);
-                                    const detectionImageOriginalElement = $("#detectionImageOriginal")
-                                    detectionImageOriginalElement.on("load", onOriginalPhotoReceived)
-                                    detectionImageOriginalElement.attr('src', latestPhotoOriginal)
-                                }
-                            }
-                        }
-                        if (data.processed && data.processed[0]) {
-                            if (latestPhotoProcessed !== data.processed[0]) {
-                                firstLoad = latestPhotoProcessed === undefined
-                                latestPhotoProcessed = data.processed[0]
-                                if (!firstLoad) {
-                                    const detectionImageProcessed = $("#detectionImageProcessed")
-                                    detectionImageProcessed.on("load", onProcessedPhotoReceived)
-                                    detectionImageProcessed.attr('src', latestPhotoProcessed)
-                                }
-                            }
-                        }
-                    }
+                if (data) {
+                    checkOriginalImageElement(data)
+                        .then(() => checkProcessedImageElement(data))
                 }
             }
         })
